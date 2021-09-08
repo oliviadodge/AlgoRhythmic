78310b61d9c955800b4111606396ea52e8ec9189
Olivia <oliviadodge@gmail.com>
2021-09-08 12:23:36 -0700

    Update BinaryTreeMaxPathSum

--- src/com/olivia/sep2021/BinaryTreeMaxPathSum.java
+++ src/com/olivia/sep2021/BinaryTreeMaxPathSum.java
@@ -2,6 +2,13 @@ package com.olivia.sep2021;
 
 import com.olivia.TreeNode;
 
+/*A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+
+The path sum of a path is the sum of the node's values in the path.
+
+Given the root of a binary tree, return the maximum path sum of any path.
+*/
+
 class BinaryTreeMaxPathSum {
 
     public static void main(String[] args) {
@@ -10,27 +17,37 @@ class BinaryTreeMaxPathSum {
         TreeNode node2 = new TreeNode(2, null, null);
         TreeNode root1 = new TreeNode(3, node1, node2);
 
-        System.out.println("median: " + thing.maxPathSum(root1));
+        Answer answer = new Answer(Integer.MIN_VALUE, Integer.MIN_VALUE);
+        System.out.println("answer: " + thing.maxPathSumAnswer(root1, answer).currentMaxPathSum);
     }
 
-    int maxPathSum = Integer.MIN_VALUE;
+    public Answer maxPathSumAnswer(TreeNode node, Answer answer) {
+        if (node == null) {
+            answer.maxPathUpToAndInclThisNode = 0;
+            return answer;
+        }
 
-    public int maxPathLeadingToNode(TreeNode node) {
-        if (node == null) return 0;
+        int maxPathLeadingToLeft = Math.max(maxPathSumAnswer(node.left, answer).maxPathUpToAndInclThisNode, 0);
 
-        int maxPathLeadingToLeft = Math.max(maxPathLeadingToNode(node.left), 0);
-
-        int maxPathLeadingToRight = Math.max(maxPathLeadingToNode(node.right), 0);
+        int maxPathLeadingToRight = Math.max(maxPathSumAnswer(node.right, answer).maxPathUpToAndInclThisNode, 0);
 
         int pathThroughRoot = node.val + maxPathLeadingToLeft + maxPathLeadingToRight;
 
-        maxPathSum = Math.max(maxPathSum, pathThroughRoot);
+        answer.currentMaxPathSum = Math.max(answer.currentMaxPathSum, pathThroughRoot);
 
-        return node.val + Math.max(maxPathLeadingToLeft, maxPathLeadingToRight);
+        answer.maxPathUpToAndInclThisNode = node.val + Math.max(maxPathLeadingToLeft, maxPathLeadingToRight);
+
+        return answer;
     }
 
-    public int maxPathSum(TreeNode root) {
-        maxPathLeadingToNode(root);
-        return maxPathSum;
+    static class Answer {
+
+        int currentMaxPathSum;
+        int maxPathUpToAndInclThisNode;
+
+        Answer(int currentMaxPathSum, int  maxLegUpToAndIncludingThisNode) {
+            this.currentMaxPathSum = currentMaxPathSum;
+            this.maxPathUpToAndInclThisNode = maxLegUpToAndIncludingThisNode;
+        }
     }
 }
