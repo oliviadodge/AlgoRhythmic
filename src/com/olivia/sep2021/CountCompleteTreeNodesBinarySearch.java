549fc4bc2ee90baa9da449e3c6efbea2bfcd3ce1
Olivia <oliviadodge@gmail.com>
2021-09-11 12:10:17 -0700

    Add count complete tree nodes with binary search

---
+++ src/com/olivia/sep2021/CountCompleteTreeNodesBinarySearch.java
@@ -0,0 +1,75 @@
+package com.olivia.sep2021;
+
+import com.olivia.TreeNode;
+
+class CountCompleteTreeNodesBinarySearch {
+
+    public static void main(String[] args) {
+        CountCompleteTreeNodesBinarySearch thing = new CountCompleteTreeNodesBinarySearch();
+
+        TreeNode node8 = new TreeNode(8, null, null);
+        TreeNode node7 = new TreeNode(7, null, null);
+        TreeNode node6 = new TreeNode(6, null, null);
+        TreeNode node5 = new TreeNode(5, null, null);
+        TreeNode node4 = new TreeNode(4, node8, null);
+        TreeNode node3 = new TreeNode(3, node6, node7);
+        TreeNode node2 = new TreeNode(2, node4, node5);
+        TreeNode node1 = new TreeNode(1, node2, node3);
+
+        System.out.println("answer: " + thing.countNodes(node1));
+    }
+
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+
+        int d = computeDepth(root);
+
+        if (d == 0) return 1;
+
+        // Last level nodes are enumerated from 0 to 2**d - 1 (left -> right).
+        // Perform binary search to check how many nodes exist.
+        int left = 1;
+        int right = (int)Math.pow(2, d) - 1;
+        int pivot;
+        while (left <= right) {
+            pivot = left + (right - left) / 2;
+            if (exists(pivot, d, root)) left = pivot + 1;
+            else right = pivot - 1;
+        }
+
+        // The tree contains 2**d - 1 nodes on the first (d - 1) levels
+        // and left nodes on the last level.
+        return (int)Math.pow(2, d) - 1 + left;
+    }
+
+
+    // Return tree depth in O(d) time.
+    public int computeDepth(TreeNode node) {
+        int d = 0;
+        while (node.left != null) {
+            node = node.left;
+            ++d;
+        }
+        return d;
+    }
+
+    // Last level nodes are enumerated from 0 to 2**d - 1 (left -> right).
+    // Return True if last level node idx exists.
+    // Binary search with O(d) complexity.
+    public boolean exists(int idx, int d, TreeNode node) {
+        int left = 0, right = (int)Math.pow(2, d) - 1;
+        int pivot;
+        for(int i = 0; i < d; ++i) {
+            pivot = left + (right - left) / 2;
+            if (idx <= pivot) {
+                node = node.left;
+                right = pivot;
+            }
+            else {
+                node = node.right;
+                left = pivot + 1;
+            }
+        }
+        return node != null;
+    }
+}
